1b0023bd1f43d9fdf68eb2224c84db76a214c3e8
vincenthrr <vishou98@hotmail.fr>
2022-01-25 16:48:31 +0100

    Update Quizz.js

---
+++ src/components/Quizz.js
@@ -0,0 +1,15 @@
+function Quiz(){
+
+    return(
+
+        <div>
+            <div id={1} class="quiz">
+                Quelle est la capitale de la France ?
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Quiz;
